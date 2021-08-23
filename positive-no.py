b3e4f0804ee0f8c6f7a77712bb279d423348f2d6
rajdeepduarah <89378584+rajdeepduarah@users.noreply.github.com>
2021-08-23 13:35:38 +0530

    Add files via upload

---
+++ positive-no.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+start=int(input("enter the start of range="))
+end=int(input("enter the end of range="))
+for i in range(start,end+1):
+    if i>=0:
+        print(i,end="")
